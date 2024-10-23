a1f94a1f4361d9bcaadafa63b7dbc8f656427355
Shannon Walker <56971170+slwalker228@users.noreply.github.com>
2024-10-23 11:23:20 -0400

    Add files via upload

---
+++ Working_draft.R
@@ -0,0 +1,335 @@
+# Practice
+
+# Shannon Walker
+# 09.27.2024
+
+library(shiny)
+library(bslib)
+library(bsicons)
+#install.packages("DT")
+library(DT)
+library(ggplot2)
+library(markdown)
+#install.packages("markdown")
+#install.packages("knitr")
+#install.packages("knitr")
+#install.packages("shinythemes")
+library(shinythemes)
+#install.packages("sortable")
+library(sortable)
+
+# May need to add shinyUI() function at top...
+
+ui <- fluidPage(theme = bs_theme(bootswatch = "minty"),
+        titlePanel("NAME OF LESSON HERE"),
+        navbarPage("Start Here",
+        tabPanel(icon("home"),
+                 fluidRow(column(tags$img(src = "pictures/Dune_pic.jpg",
+                                          width = "200px",
+                                          height = "260px"),
+                                          width = 2
+                                          ),
+                          column(
+                            
+                            br(),
+                            
+                            p("Welcome to the lesson! Today, we will be exploring how climate is changing the coastal landscape. We will hear from Dr. Natasha Woods about her
+                              research about how climate is driving the expansion of a native shrub on barrier islands along the coast of Virginia, USA. Next, we will explore
+                              the real data she and her co-author collected. You will create figures from the data, run statistical tests, and interpret the results. Lastly, we
+                              will hear from Dr. Woods about her experience as a scientist."),
+                            
+                            br(),
+                            
+                            p("There is also an adpatation for this module which invites you to code in R to run the statistics and generate figures. If you don't 
+                              know how to code or have limited experience", strong("don't worry!"), "The module uses a fill-in-the-blanks format and will walk you through the code 
+                              step-by-step."),
+                            
+                            width = 8
+                          ),
+                          column(
+                            br(),
+                            p("The data used in this lesson are curated by the Long Term Ecological Research Network whose mission is to make data availble online with as few 
+                              restrictions as possible. Learn more about the Long Term Ecological Research Network by clicking",
+                              a(href = "https://lternet.edu/about/", "this link", target = "_blank", style = "color:black"),
+                              width = 2),
+                            width = 2
+                          )
+                                 ),
+                 
+                 hr(),
+                 fluidRow(
+                   column(
+                   br(),
+                   p("BACKGROUND INFORMATION HERE. EXPLANATION OF PROBLEM.",
+                     width = 12),
+                   width = 12
+                 )
+                 ),
+                 
+                 hr(),
+                 p(em("Developed by"),br("Shannon Walker, Natasha Woods, and Alycia Lackey"),style="text-align:center; font-family: times")),
+        
+        ################### Part 1 ######################
+        
+        tabPanel("Part I",
+                 fluidRow(column(width = 2),
+                          column(
+                            br(),
+                            p("This section will be for the Pearson Correlation",
+                              align = "center"),
+                            width = 8
+                          ),
+                          column(width = 2))
+                 ),
+
+        ################### Part 2 ######################
+        
+        tabPanel("Part II",
+                 fluidRow(column(width = 2),
+                          column(
+                            br(),
+                            p("This section will be for the one-way ANOVA",
+                              align = "center"),
+                            width = 8
+                          ),
+                          column(width = 2))
+        ),
+        
+        ################### Part 3 ######################
+        
+        tabPanel("Part III",
+                 fluidRow(column(width = 2),
+                          column(
+                            br(),
+                            p("This section will be for the linear regression",
+                              align = "center"),
+                            width = 8
+                          ),
+                          column(width = 2))
+        ),
+        
+        ################### Part 4 ######################
+        
+        tabPanel("Part IV",
+                 fluidRow(column(width = 2),
+                          column(
+                            br(),
+                            p("This section will be for final interpretation",
+                              align = "center"),
+                            width = 8
+                          ),
+                          column(width = 2))
+        ),
+        
+        ################### Part 5 ######################
+        
+        tabPanel("Part V",
+                 fluidRow(column(width = 2),
+                          column(
+                            br(),
+                            p("This section will be for the final part of the interview",
+                              align = "center"),
+                            width = 8
+                          ),
+                          column(width = 2))
+        )
+                 
+        )
+)
+
+
+# may need to add shinyServer() around this at end
+
+server <- function(input, output){
+  
+}
+
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+###############################################################################
+
+## Old junk below here ##
+
+###############################################################################
+
+
+
+ui <- navbarPage(
+  theme = bs_theme(bootswatch = "minty"),
+    title = "Coastal Crisis",
+    inverse = TRUE,
+    nav_panel(title = "Start Here", 
+              p("What might you and a shrub have in common? A love of the beach!"),
+              card(
+                card_header(),
+                card_body(
+                  markdown("[Read the Full Article Here!](https://link.springer.com/article/10.1007/s11258-024-01453-2)")
+                ),
+                  value_box(
+                    title = "Value box",
+                    value = 100,
+                    showcase = bsicons::bs_icon("bar-chart")),
+                  card(
+                    card_header("Card header"),
+                    "Card body"
+                  ),
+                  card(
+                    card_header("Card header 2"),
+                    "Just checking this out"
+                  )
+                )),
+    nav_panel(title = "Coding Time: Pearson's Correlation", 
+              p("Second page content."),
+              ,
+
+              card(
+                card_body(
+                   helpText("Some text and then ", code("some code"), ".")
+                )
+              ),
+              card(
+                card_header(),
+                card_body(
+                  column(4,
+                         includeMarkdown("include.md")
+                  )
+                ))
+    ),
+    nav_panel(title = "Three", 
+              p("Third page content.")),
+    nav_spacer(),
+    nav_menu(
+      title = "Want to learn more?",
+      align = "right",
+      nav_item(tags$a("Article: Protectors of the Coast", href = "https://magazine.moravian.edu/protectors-of-the-coast/")),
+      nav_item(tags$a("What is an R Shiny App?", href = "https://shiny.posit.co"))
+    )
+)
+
+# Define server logic ----
+server <- function(input, output) {
+  output$pdfview <- renderUI({
+    tags$iframe(style="height:600px; width:100%", src="Woods_Zinnert_2024.pdf")
+  })
+}
+
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
+
+
+
+
+ui <- page_navbar(
+  title = "Coastal Crisis",
+inverse = TRUE,
+  nav_panel(title = "Start Here", 
+            p("What might you and a shrub have in common? A love of the beach!"),
+            theme = bs_theme(bootswatch = "minty"),
+            card(
+              card_header(
+              class = "bg-dark",
+              "Background"),
+              card_body(
+              markdown("[Read the Full Article Here!](https://link.springer.com/article/10.1007/s11258-024-01453-2)")
+            )),
+            ),
+  nav_panel(title = "Two", p("Second page content."),
+            card(
+              card_header(),
+              card_body(
+                column(4,
+                       includeMarkdown("include.md")
+              )
+            ))
+            ),
+  nav_panel(title = "Three", p("Third page content."),
+            card(
+              card_body(
+                if(interactive()) {
+                  sortable::bucket_list(
+                    header = "This is a bucket list. You can drag items between the lists.",
+                    add_rank_list(
+                      text = "Drag from here",
+                      labels = c("a", "bb", "ccc")
+                    ),
+                    add_rank_list(
+                      text = "to here",
+                      labels = NULL
+                    )
+                  )
+                }
+              )
+            )),
+  nav_spacer(),
+  nav_menu(
+    title = "Links",
+    align = "right",
+    nav_item(tags$a("News Story", href = "https://magazine.moravian.edu/protectors-of-the-coast/")),
+    nav_item(tags$a("What is an R Shiny App?", href = "https://shiny.posit.co"))
+  )
+)
+
+
+server <- function(input, output) {
+  output$pdfview <- renderUI({
+    tags$iframe(style="height:600px; width:100%", src="Woods_Zinnert_2024.pdf")
+  })
+}
+
+
+shinyApp(ui, server)
+
+
+
+### Other junk ###
+
+
+fillRow(sidebarLayout(
+  sidebarPanel(
+    h1("Side panel"),
+    bucket_list(
+      header = NULL,
+      group_name = "bucket_list_group",
+      orientation = "horizontal",
+      add_rank_list(
+        text = "Drag items from here ...",
+        labels = list("A",
+                      "B",
+                      "C"),
+        input_id = "rank_list_1"
+      )
+    ),
+    br(),
+    verbatimTextOutput("results_2"),
+    width = 6
+  ),
+  
+  mainPanel(
+    br(),
+    h1("Main panel"),
+    sortable::bucket_list(
+      header = NULL,
+      group_name = "bucket_list_group",
+      orientation = "horizontal",
+      add_rank_list(
+        text = "... to here",
+        labels = NULL,
+        input_id = "rank_list_2"
+      )
+    ),
+    br(),
+    verbatimTextOutput("results_1"),
+    width = 6
+  ),
+  position = "right",
+  fluid = FALSE
+))
